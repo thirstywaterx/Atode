50ef694b20d4e92729f482fee45ebd50b32ced0b
ThirstyWater <thistywaerx@outlook.com>
2025-07-28 16:14:53 +0800

    改进

--- node/config/config.js
+++ node/config/config.js
@@ -2,6 +2,6 @@ module.exports = {
     gemini: {
         apiKey: 'AIzaSyBs9Xh3dsOBFBj4fuvynYxdFXRKoVXs-x4',
         baseUrl: 'generativelanguage.googleapis.com',
-        model: 'gemini-1.5-flash' // 使用支持流式传输的flash模型
+        model: 'gemini-2.5-pro' // 使用支持流式传输的flash模型
     }
 };
